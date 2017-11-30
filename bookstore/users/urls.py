20ae2ecc463cc92d45ad6af6b186cdc3a386090f
Zuo Yuan <zuoyuantc@126.com>
2017-11-30 23:56:05 +0800

    注册功能实现

--- bookstore/users/urls.py
+++ bookstore/users/urls.py
@@ -3,4 +3,5 @@ from users import views
 
 urlpatterns = [
     url(r'^register/$', views.register, name='register'), # 用户注册
+    url(r'^register_handle/$', views.register_handle, name='register_handle'), # 用户注册处理
 ]
