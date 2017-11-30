740dd9ea0b2af0c3001e8fd0b555b03e4da3750a
Zuo Yuan <zuoyuantc@126.com>
2017-11-30 15:03:13 +0800

    修改配置文件，将默认sqlite改为mysql

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bj15_dailyfresh',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
