eb62572a9e8079468f160047d887483a7110cd50
Zuo Yuan <zuoyuantc@126.com>
2017-12-01 22:07:27 +0800

    实现登陆功能以及校验功能

--- bookstore/users/urls.py
+++ bookstore/users/urls.py
@@ -4,5 +4,6 @@ from users import views
 urlpatterns = [
     url(r'^register/$', views.register, name='register'), # 用户注册
     url(r'^register_handle/$', views.register_handle, name='register_handle'), # 用户注册处理
-    url(r'^login/$', views.login, name='login') # 显示登陆页面
+    url(r'^login/$', views.login, name='login'), # 显示登陆页面
+    url(r'^login_check/$', views.login_check, name='login_check'), # 用户登录校验
 ]
--- bookstore/users/views.py
+++ bookstore/users/views.py
@@ -44,6 +44,48 @@ def login(request):
 
     return render(request, 'users/login.html', context)
 
+def login_check(request):
+    '''进行用户登录校验'''
+    # 1.获取数据
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    remember = request.POST.get('remember')
+
+    # 2.数据校验
+    if not all([username, password, remember]):
+        # 有数据为空
+        return JsonResponse({'res': 2})
+
+    # 3.进行处理:根据用户名和密码查找账户信息
+    passport = Passport.objects.get_one_passport(username=username, password=password)
+
+    if passport:
+        # 用户名密码正确
+        # 获取session中的url_path
+        # if request.session.has_key('url_path'):
+        #     next_url = request.session.get('url_path')
+        # else:
+        #     next_url = reverse('books:index')
+        next_url = request.session.get('url_path', reverse('books:index')) # /user/
+        jres = JsonResponse({'res': 1, 'next_url': next_url})
+
+        # 判断是否需要记住用户名
+        if remember == 'true':
+            # 记住用户名
+            jres.set_cookie('username', username, max_age=7*24*3600)
+        else:
+            # 不要记住用户名
+            jres.delete_cookie('username')
+
+        # 记住用户的登录状态
+        request.session['islogin'] = True
+        request.session['username'] = username
+        request.session['passport_id'] = passport.id
+        return jres
+    else:
+        # 用户名或密码错误
+        return JsonResponse({'res': 0})
+
 
 
 
