4bdcd93d53943b9dad46cb2d69f890ef8d3c1df1
Zuo Yuan <zuoyuantc@126.com>
2017-12-02 15:56:05 +0800

    列表页功能实现

--- bookstore/books/urls.py
+++ bookstore/books/urls.py
@@ -4,4 +4,5 @@ from books import views
 urlpatterns = [
     url(r'^$', views.index, name='index'), # 首页
     url(r'books/(?P<books_id>\d+)/$', views.detail, name='detail'), # 详情页
+    url(r'^list/(?P<type_id>\d+)/(?P<page>\d+)/$', views.list, name='list'), # 列表页
 ]
--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -58,3 +58,63 @@ def detail(request, books_id):
     # 使用模板
     return render(request, 'books/detail.html', context)
 
+# 商品种类 页码 排序方式
+# /list/(种类id)/(页码)/?sort=排序方式
+def list(request, type_id, page):
+    '''商品列表页面'''
+    # 获取排序方式
+    sort = request.GET.get('sort', 'default')
+
+    # 判断type_id是否合法
+    if int(type_id) not in BOOKS_TYPE.keys():
+        return redirect(reverse('books:index'))
+
+    # 根据商品种类id和排序方式查询数据
+    books_li = Books.objects.get_books_by_type(type_id=type_id, sort=sort)
+
+    # 分页
+    paginator = Paginator(books_li, 1)
+
+    # 获取分页之后的总页数
+    num_pages = paginator.num_pages
+
+    # 取第page页数据
+    if page == '' or int(page) > num_pages:
+        page = 1
+    else:
+        page = int(page)
+
+    # 返回值是一个Page类的实例对象
+    books_li = paginator.page(page)
+
+    # 进行页码控制
+    # 1.总页数<5, 显示所有页码
+    # 2.当前页是前3页，显示1-5页
+    # 3.当前页是后3页，显示后5页 10 9 8 7
+    # 4.其他情况，显示当前页前2页，后2页，当前页
+    if num_pages < 5:
+        pages = range(1, num_pages+1)
+    elif page <= 3:
+        pages = range(1, 6)
+    elif num_pages - page <= 2:
+        pages = range(num_pages-4, num_pages+1)
+    else:
+        pages = range(page-2, page+3)
+
+    # 新品推荐
+    books_new = Books.objects.get_books_by_type(type_id=type_id, limit=2, sort='new')
+
+    # 定义上下文
+    type_title = BOOKS_TYPE[int(type_id)]
+    context = {
+        'books_li': books_li,
+        'books_new': books_new,
+        'type_id': type_id,
+        'sort': sort,
+        'type_title': type_title,
+        'pages': pages
+    }
+
+    # 使用模板
+    return render(request, 'books/list.html', context)
+
