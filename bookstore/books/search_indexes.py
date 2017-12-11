3d7a74cf27b92ee88fcb7fc8fe110882e7e02302
Zuo Yuan <zuoyuantc@126.com>
2017-12-11 21:34:59 +0800

    修复搜索引擎bug

--- bookstore/books/search_indexes.py
+++ bookstore/books/search_indexes.py
@@ -1,5 +1,5 @@
 from haystack import indexes
-from boods.models import Boods
+from books.models import Books
 
 
 # 指定对于某个类的某些数据建立索引, 一般类名:模型类名+Index
@@ -8,7 +8,7 @@ class BooksIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
-        return Boods
+        return Books
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
